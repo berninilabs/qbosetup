b325c51f993c843e3a2e256500290a248689e38b
Sang Venkatraman <60385589+berninilabs@users.noreply.github.com>
2022-08-29 20:23:21 -0400

    Added getting vendors

--- routes/api_call.js
+++ routes/api_call.js
@@ -49,6 +49,52 @@ router.get("/companyinfo", function (req, res) {
   });
 });
 
+router.get("/vendor", function (req, res) {
+  var token = tools.getToken(req.session);
+  if (!token) return res.json({ error: "Not authorized" });
+  if (!req.session.realmId)
+    return res.json({
+      error:
+        "No realm ID.  QBO calls only work if the accounting scope was passed!",
+    });
+
+  // Set up API call (with OAuth2 accessToken)
+  var url =
+    config.api_uri +
+    req.session.realmId +
+    "/query?query=select * from vendor order by DisplayName asc";
+  console.log("Making API call to: " + url);
+  var requestObj = {
+    url: url,
+    headers: {
+      Authorization: "Bearer " + token.accessToken,
+      Accept: "application/json",
+    },
+  };
+
+  // Make API call
+  request(requestObj, function (err, response) {
+    // Check if 401 response was returned - refresh tokens if so!
+    tools.checkForUnauthorized(req, requestObj, err, response).then(
+      function ({ err, response }) {
+        if (err || response.statusCode != 200) {
+          console.log(
+            `Got response status code ${response.statusCode} with err: ${err}`
+          );
+          return res.json({ error: err, statusCode: response.statusCode });
+        }
+
+        // API Call was a success!
+        res.json(JSON.parse(response.body));
+      },
+      function (err) {
+        console.log(err);
+        return res.json(err);
+      }
+    );
+  });
+});
+
 router.get("/update_vendors", function (req, res) {
   var token = tools.getToken(req.session);
   if (!token) return res.json({ error: "Not authorized" });
